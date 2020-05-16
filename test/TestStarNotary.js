8af575a699441fc47a22598aad4c968290ee398f
heyoka9814 <65400466+heyoka9814@users.noreply.github.com>
2020-05-16 15:40:44 +0530

    Update TestStarNotary.js

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -1,125 +1,252 @@
-const StarNotary = artifacts.require("StarNotary");
+/* eslint no-undef:0 */
+/* eslint func-names:0 */
+/* eslint prefer-arrow-callback:0 */
 
-var accounts;
-var owner;
+const StarNotary = artifacts.require('StarNotary');
+const REVERT_ERROR_MSG = 'VM Exception while processing transaction: revert';
 
-contract('StarNotary', (accs) => {
-    accounts = accs;
-    owner = accounts[0];
-});
+contract('StarNotary', (accounts) => {
+  beforeEach(async function () {
+    this.contract = await StarNotary.new({ from: accounts[0] });
+  });
 
-it('can Create a Star', async() => {
-    let tokenId = 1;
-    let instance = await StarNotary.deployed();
-    await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]})
-    assert.equal(await instance.tokenIdToStarInfo.call(tokenId), 'Awesome Star!')
-});
+  describe('can create a star', () => {
+    it('can create a star and get its details', async function () {
+      await this.contract.createStar(
+        'awesome star!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+        'awesome star story', 1, { from: accounts[0] });
+      assert.deepEqual(
+        await this.contract.tokenIdToStarInfo(1),
+        [
+          'awesome star!', 'dec_121.874', 'mag_245.978',
+          'ra_032.155', 'awesome star story',
+        ]);
+    });
+    it('can create two stars with different coordinates', async function () {
+      await this.contract.createStar(
+        'awesome star #1!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+        'awesome star story #1', 1, { from: accounts[0] });
+      assert.deepEqual(
+        await this.contract.tokenIdToStarInfo(1),
+        [
+          'awesome star #1!', 'dec_121.874', 'mag_245.978',
+          'ra_032.155', 'awesome star story #1',
+        ]);
 
-it('lets user1 put up their star for sale', async() => {
-    let instance = await StarNotary.deployed();
-    let user1 = accounts[1];
-    let starId = 2;
-    let starPrice = web3.utils.toWei(".01", "ether");
-    await instance.createStar('awesome star', starId, {from: user1});
-    await instance.putStarUpForSale(starId, starPrice, {from: user1});
-    assert.equal(await instance.starsForSale.call(starId), starPrice);
-});
+      await this.contract.createStar(
+        'awesome star #2!', 'dec_121.875', 'mag_245.979', 'ra_032.156',
+        'awesome star story #2', 2, { from: accounts[1] });
+      assert.deepEqual(
+        await this.contract.tokenIdToStarInfo(2),
+        [
+          'awesome star #2!', 'dec_121.875', 'mag_245.979',
+          'ra_032.156', 'awesome star story #2',
+        ]);
+    });
+    it('can\'t create two stars with same coordinates', async function () {
+      await this.contract.createStar(
+        'awesome star #1!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+        'awesome star story #1', 1, { from: accounts[0] });
 
-it('lets user1 get the funds after the sale', async() => {
-    let instance = await StarNotary.deployed();
-    let user1 = accounts[1];
-    let user2 = accounts[2];
-    let starId = 3;
-    let starPrice = web3.utils.toWei(".01", "ether");
-    let balance = web3.utils.toWei(".05", "ether");
-    await instance.createStar('awesome star', starId, {from: user1});
-    await instance.putStarUpForSale(starId, starPrice, {from: user1});
-    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user1);
-    await instance.buyStar(starId, {from: user2, value: balance});
-    let balanceOfUser1AfterTransaction = await web3.eth.getBalance(user1);
-    let value1 = Number(balanceOfUser1BeforeTransaction) + Number(starPrice);
-    let value2 = Number(balanceOfUser1AfterTransaction);
-    assert.equal(value1, value2);
-});
+      let errorThrown;
 
-it('lets user2 buy a star, if it is put up for sale', async() => {
-    let instance = await StarNotary.deployed();
-    let user1 = accounts[1];
-    let user2 = accounts[2];
-    let starId = 4;
-    let starPrice = web3.utils.toWei(".01", "ether");
-    let balance = web3.utils.toWei(".05", "ether");
-    await instance.createStar('awesome star', starId, {from: user1});
-    await instance.putStarUpForSale(starId, starPrice, {from: user1});
-    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
-    await instance.buyStar(starId, {from: user2, value: balance});
-    assert.equal(await instance.ownerOf.call(starId), user2);
-});
+      try {
+        await this.contract.createStar(
+          'awesome star #2!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+          'awesome star story #2', 2, { from: accounts[1] });
+      } catch (error) {
+        errorThrown = error;
+      }
 
-it('lets user2 buy a star and decreases its balance in ether', async() => {
-    let instance = await StarNotary.deployed();
-    let user1 = accounts[1];
-    let user2 = accounts[2];
-    let starId = 5;
-    let starPrice = web3.utils.toWei(".01", "ether");
-    let balance = web3.utils.toWei(".05", "ether");
-    await instance.createStar('awesome star', starId, {from: user1});
-    await instance.putStarUpForSale(starId, starPrice, {from: user1});
-    let balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user2);
-    const balanceOfUser2BeforeTransaction = await web3.eth.getBalance(user2);
-    await instance.buyStar(starId, {from: user2, value: balance, gasPrice:0});
-    const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
-    let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
-    assert.equal(value, starPrice);
-});
+      assert.notEqual(errorThrown, undefined, 'Error should be thrown');
+      assert.isAbove(
+        errorThrown.message.search(REVERT_ERROR_MSG),
+        -1, `Error: ${REVERT_ERROR_MSG}`);
+    });
+  });
 
-// Implement Task 2 Add supporting unit tests
+  describe('can buy and sell stars', () => {
+    const user1 = accounts[1];
+    const user2 = accounts[2];
 
-it('can add the star name properly', async() => {
-    // 1. create a Star with different tokenId
-    //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
-    let tokenId = 6;
-    let instance = await StarNotary.deployed();
-    await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]});
-    assert.equal('AllStar', await instance.name.call());
-});
+    const tokenId = 1;
+    const starPrice = web3.toWei(0.01, 'ether');
 
-it('can add the star symbol properly', async() => {
-    // 1. create a Star with different tokenId
-    //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
-    let tokenId = 7;
-    let instance = await StarNotary.deployed();
-    await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]});
-    assert.equal('AST', await instance.symbol.call());
-});
+    beforeEach(async function () {
+      await this.contract.createStar(
+        'awesome star #1!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+        'awesome star story #1', tokenId, { from: user1 });
+    });
 
-it('lets 2 users exchange stars', async() => {
-    // 1. create 2 Stars with different tokenId
-    // 2. Call the exchangeStars functions implemented in the Smart Contract
-    // 3. Verify that the owners changed
-    let tokenId1 = 8;
-    let tokenId2 = 9;
-    let instance = await StarNotary.deployed();
-    await instance.createStar('Awesome Star!', tokenId1, {from: accounts[0]});
-    await instance.createStar('Equally Awesome Star!', tokenId2, {from: accounts[1]});
-    await instance.exchangeStars(tokenId1, tokenId2);
-    assert.equal(await instance.ownerOf(tokenId1), accounts[1]);
-    assert.equal(await instance.ownerOf(tokenId2), accounts[0]);
-});
+    it('can put up star for sale', async function () {
+      assert.equal(await this.contract.ownerOf(tokenId), user1);
+      await this.contract.putStarUpForSale(
+        tokenId, starPrice, { from: user1 });
 
-it('lets a user transfer a star', async() => {
-    // 1. create a Star with different tokenId
-    // 2. use the transferStar function implemented in the Smart Contract
-    // 3. Verify the star owner changed.
-    let tokenId = 10;
-    let instance = await StarNotary.deployed();
-    await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]});
-    await instance.transferStar(accounts[1], tokenId, {from: accounts[0]});
-    assert.equal(await instance.ownerOf(tokenId), accounts[1]);
-});
+      assert.equal(await this.contract.starsForSale(tokenId), starPrice);
+    });
 
-it('lookUptokenIdToStarInfo test', async() => {
-    // 1. create a Star with different tokenId
-    // 2. Call your method lookUptokenIdToStarInfo
-    // 3. Verify if you Star name is the same
+    describe('can buy a star that was put up for sale', () => {
+      beforeEach(async function () {
+        await this.contract.putStarUpForSale(
+          tokenId, starPrice, { from: user1 });
+      });
+
+      it('user2 is the owner of star after they buy it', async function () {
+        await this.contract.buyStar(
+          tokenId, { from: user2, value: starPrice, gasPrice: 0 });
+        assert.equal(await this.contract.ownerOf(tokenId), user2);
+      });
+
+      it('user2 ether balance changed correctly', async function () {
+        const overpaidAmount = web3.toWei(0.05, 'ether');
+        const balanceBeforeTransaction = web3.eth.getBalance(user2);
+        await this.contract.buyStar(
+          tokenId, { from: user2, value: overpaidAmount, gasPrice: 0 });
+        const balanceAfterTransaction = web3.eth.getBalance(user2);
+
+        assert.equal(
+          balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice);
+      });
+    });
+  });
+
+  describe('can check existence of a star', () => {
+    const tokenId = 1;
+    const nonexistentStarId = 2;
+
+    beforeEach(async function () {
+      await this.contract.createStar(
+        'awesome star #1!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+        'awesome star story #1', tokenId, { from: accounts[0] });
+    });
+
+    it('can check if star exists', async function () {
+      assert.equal(await this.contract.checkIfStarExist(tokenId), true);
+    });
+
+    it('can check if star doesn\'t exist', async function () {
+      assert.equal(
+        await this.contract.checkIfStarExist(nonexistentStarId), false);
+    });
+  });
+
+  describe('can mint a token', () => {
+    const tokenId = 1;
+    const user = accounts[0];
+
+    let tx;
+
+    beforeEach(async function () {
+      tx = await this.contract.mint(tokenId, { from: user });
+    });
+
+    it('owner of tokenId is user', async function () {
+      assert.equal(await this.contract.ownerOf(tokenId), user);
+    });
+
+    it('balance of user is incremented by 1', async function () {
+      const balance = await this.contract.balanceOf(user);
+      assert.equal(balance.toNumber(), 1);
+    });
+
+    it('emits correct event during minting of new token', async function () {
+      assert.equal(tx.logs[0].event, 'Transfer');
+    });
+  });
+
+  describe('can safely transfer token', () => {
+    const tokenId = 1;
+    const user1 = accounts[0];
+    const user2 = accounts[1];
+
+    let tx;
+
+    beforeEach(async function () {
+      await this.contract.mint(tokenId, { from: user1 });
+      tx = await this.contract.safeTransferFrom(
+        user1, user2, tokenId, { from: user1 });
+    });
+
+    it('token has new owner', async function () {
+      assert.equal(await this.contract.ownerOf(tokenId), user2);
+    });
+
+    it('emits the correct event', async function () {
+      assert.equal(tx.logs[0].event, 'Transfer');
+      assert.equal(tx.logs[0].args.tokenId, tokenId);
+      assert.equal(tx.logs[0].args.to, user2);
+      assert.equal(tx.logs[0].args.from, user1);
+    });
+  });
+
+  describe('can grant approval to transfer', () => {
+    const tokenId = 1;
+    const user1 = accounts[0];
+    const user2 = accounts[1];
+
+    let tx;
+
+    beforeEach(async function () {
+      await this.contract.mint(tokenId, { from: user1 });
+      tx = await this.contract.approve(user2, tokenId, { from: user1 });
+    });
+
+    it('user can be set as an approved address', async function () {
+      assert.equal(await this.contract.getApproved(tokenId), user2);
+    });
+
+    it('approved user can receive transfer', async function () {
+      await this.contract.transferFrom(
+        user1, user2, tokenId, { from: user2 });
+      assert.equal(await this.contract.ownerOf(tokenId), user2);
+    });
+
+    it('emits the correct event during approval', async function () {
+      assert.equal(tx.logs[0].event, 'Approval');
+    });
+  });
+
+  describe('can set an operator', () => {
+    const operator = accounts[0];
+    const tokenId = 1;
+    const user = accounts[1];
+
+    beforeEach(async function () {
+      await this.contract.mint(tokenId, { from: user });
+      await this.contract.setApprovalForAll(operator, true, { from: user });
+    });
+
+    it('can set an operator and check approval status', async function () {
+      assert.equal(
+        await this.contract.isApprovedForAll(user, operator), true);
+    });
+  });
+
+  describe('can utilize star contract mappings', () => {
+    const tokenId = 1;
+    const starPrice = web3.toWei(0.01, 'ether');
+    const user = accounts[0];
+
+    beforeEach(async function () {
+      await this.contract.createStar(
+        'awesome star!', 'dec_121.874', 'mag_245.978', 'ra_032.155',
+        'awesome star story', tokenId, { from: user });
+    });
+
+    it('can get price of a star for sale', async function () {
+      await this.contract.putStarUpForSale(
+        tokenId, starPrice, { from: user });
+      assert.equal(await this.contract.starsForSale(tokenId), starPrice);
+    });
+
+    it('can get star info by tokenId', async function () {
+      assert.deepEqual(
+        await this.contract.tokenIdToStarInfo(1),
+        [
+          'awesome star!', 'dec_121.874', 'mag_245.978',
+          'ra_032.155', 'awesome star story',
+        ]);
+    });
+  });
 });
